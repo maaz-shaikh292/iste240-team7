da0f685d8e9fec24191ec563b657dd9750780b45
Amir Faraji <amirfa.1.ir@gmail.com>
2026-03-13 17:11:30 +0400

    Save current work on feature-car-amir

---
+++ src/main/java/org/example/car_rental_project/controllers/CarController.java
@@ -0,0 +1,8 @@
+package org.example.car_rental_project.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CarController {
+
+}
--- src/main/java/org/example/car_rental_project/services/RentalService.java
+++ src/main/java/org/example/car_rental_project/services/RentalService.java
@@ -1,9 +1,6 @@
 package org.example.car_rental_project.services;
 
-// Maaz Shaikh, UID-421007607
-
-import org.example.car_rental_project.models.Booking;
-import org.example.car_rental_project.models.Customer;
+import org.example.car_rental_project.models.Car;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,41 +8,16 @@ import java.util.List;
 
 @Service
 public class RentalService {
-
-    private List<Customer> customers = new ArrayList<>();
-    private List<Booking> bookings = new ArrayList<>();
+    private List<Car> cars = new ArrayList<>();
 
     public RentalService() {
-
-        Customer sampleCustomer = new Customer("maaz_user", "ritabc123", "mas9236@rit.edu", "123456");
-        customers.add(sampleCustomer);
-
-        Booking sampleBooking = new Booking("2026-03-15", "2026-03-20", 550.00, true, sampleCustomer);
-        bookings.add(sampleBooking);
+        var car1 = new Car("Benz", "E63", 2000);
+        var car2 = new Car("BMW", "X6", 2500);
+        var car3 = new Car("Nissan", "Patrol", 1500);
     }
 
-    public List<Customer> getAllCustomers() {
-        return customers;
-    }
 
-    public void addCustomer(Customer customer) {
-        customers.add(customer);
-    }
 
-    public Customer getCustomerByUsername(String username) {
-        for (Customer c : customers) {
-            if (c.getUsername().equalsIgnoreCase(username)) {
-                return c;
-            }
-        }
-        return null;
-    }
 
-    public List<Booking> getAllBookings() {
-        return bookings;
-    }
 
-    public void addBooking(Booking booking) {
-        bookings.add(booking);
-    }
 }
