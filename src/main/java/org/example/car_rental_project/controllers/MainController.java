2b19f11bbed20ac0bcd6362c2b9534b23027c2a0
Amir Faraji <amirfa.1.ir@gmail.com>
2026-04-24 21:50:40 +0400

    Controller pages are now all under one unified page.

--- src/main/java/org/example/car_rental_project/controllers/MainController.java
+++ src/main/java/org/example/car_rental_project/controllers/MainController.java
@@ -1,28 +1,30 @@
-package org.example.car_rental_project.controllers;
-
+// Amir Faraji, UID-754006884
 // Maaz Shaikh, UID-421007607
+// Alexander Ougnitch, UID-759002953
 
+package org.example.car_rental_project.controllers;
 import org.example.car_rental_project.models.Booking;
+import org.example.car_rental_project.models.Car;
 import org.example.car_rental_project.models.Customer;
+import org.example.car_rental_project.models.Review;
 import org.example.car_rental_project.services.RentalService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @Controller
-public class CustomerBookingController {
+public class MainController {
 
     private final RentalService rentalService;
 
-    public CustomerBookingController(RentalService rentalService) {
-        this.rentalService = rentalService;
+    public MainController (RentalService rentalService){
+        this.rentalService=rentalService;
     }
 
-
+    //maaz
     @GetMapping("/customers")
     public String showCustomers(Model model) {
         model.addAttribute("customers", rentalService.getAllCustomers());
@@ -66,6 +68,50 @@ public class CustomerBookingController {
 
         rentalService.addBooking(newBooking);
         return "redirect:/add/success/booking";
+    }
+
+    // Alexander
+    @GetMapping("/reviews")
+    public String showReviews(Model model) {
+        model.addAttribute("reviews", rentalService.getAllReviews());
+        return "reviews-list";
+    }
+
+    @GetMapping("/reviews/add")
+    public String showAddReviewsForm() {
+        return "reviews-form";
+    }
+
+    @PostMapping("/review/add")
+    public String addReview(Review review) {
+        rentalService.addReview(review);
+        return "redirect:/add/success/review";
+    }
+
+    //Amir
+    @GetMapping("/cars")
+    public String showCarsRentals(Model model){
+        model.addAttribute("cars", rentalService.getAllCars());
+        return "car_rentals";
+    }
+
+    @GetMapping("/cars/add")
+    public  String showCarForm(){
+        return "car_form";
+    }
+
+    @PostMapping("/cars/add")
+    public String addCar( Car car){
+        rentalService.addCar(car);
+        return "redirect:/cars";
+    }
+
+    // sucess message redirecrtion mapping
+    @GetMapping("/add/success/{type}")
+    public String showSuccess(@PathVariable String type, Model model) {
+
+        model.addAttribute("successMessage", "Success! " + type + " was added successfully!");
+        return "success";
     }
 
 
